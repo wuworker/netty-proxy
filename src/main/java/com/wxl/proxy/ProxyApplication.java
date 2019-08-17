c013a7ff11599560b93f9cd359ff19bd2403f51a
wuworker <wuxingle17@163.com>
2019-08-17 10:46:19 +0800

    初始化

---
+++ src/main/java/com/wxl/proxy/ProxyApplication.java
@@ -0,0 +1,16 @@
+package com.wxl.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create by wuxingle on 2019/8/17
+ */
+@SpringBootApplication
+public class ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+}
